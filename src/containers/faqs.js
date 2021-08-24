e5bef21b3c4c5fea1913050c3159fe8d61da9fab
Pavan S Patil <psp3075@gmail.com>
2021-08-24 09:05:41 +0530

    added input form on home

--- src/containers/faqs.js
+++ src/containers/faqs.js
@@ -1,6 +1,7 @@
 import React from "react";
 import faqData from "../fixtures/faqs.json";
 import Accordion from "../components/accordion/index";
+import OptForm from "./../components/opt-form/index";
 
 export function FaqsContainer() {
   return (
@@ -12,6 +13,13 @@ export function FaqsContainer() {
           <Accordion.Body>{item.body}</Accordion.Body>
         </Accordion.Item>
       ))}
+      <OptForm>
+        <OptForm.Input placeholder="Email Address" />
+        <OptForm.Button>Try it now</OptForm.Button>
+        <OptForm.Text>
+          Ready to watch? Enter your email to create or restart your membership.
+        </OptForm.Text>
+      </OptForm>
     </Accordion>
   );
 }
