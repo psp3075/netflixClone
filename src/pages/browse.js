5d5f75398edded976b15cf057187c35c4a69b8d3
Pavan S Patil <psp3075@gmail.com>
2021-09-09 08:48:12 +0530

    extract genres

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,6 +1,11 @@
 import React from "react";
+import useContent from "../hooks/useContent";
+import selection from "../utils/selection";
 
 const Browse = () => {
+  const { series } = useContent("series");
+  const { films } = useContent("films");
+  const slides = selection({ series, films });
   return <div>Browse</div>;
 };
 
--- src/pages/signup.js
+++ src/pages/signup.js
@@ -25,7 +25,7 @@ const Signup = () => {
         result.user
           .updateProfile({
             displayName: userName,
-            photoUrl: Math.floor(Math.random() * 5) + 1,
+            photoURL: Math.floor(Math.random() * 5) + 1,
           })
           .then(() => {
             history.push(ROUTES.BROWSE);
